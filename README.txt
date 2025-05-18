67999f55f81b7f344c107f5d2cb9e1601fe5b575
silas.dev <tiilas777@gmail.com>
2025-05-18 18:40:06 -0300

    finalizado

---
+++ README.txt
@@ -0,0 +1,13 @@
+#Script feito no shell para poder inicializar um servidor web do zero utilizando o apache 
+
+
+
+
+
+
+
+
+
+
+
+
--- script.sh
+++ script.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "Script para criação de um servidor web"
+echo "Atualizando pacotes..."
+
+sudo apt update && sudo apt upgrade
+
+echo "Instalando apache..."
+
+sudo apt install apache2 -y
+
+echo "Criando a mainpage..." | sudo tee /var/www/html/index.html
+
+echo "Iniciando apache..."
+
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+echo "Status..."
+
+sudo systemctl status apache2
